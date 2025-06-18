fdd8255b67d4e71c090e9756c7e2659eac865833
Alexandre-Caby <alexandre.caby@ig2i.centralelille.fr (GitHub)>
2025-06-18 08:41:42 +0200

    initial commit

---
+++ SimpleWMS/app/models/__init__.py
@@ -0,0 +1,18 @@
+from .mission import  Mission
+from .article import Article
+from .agent import Agent
+from .commande import Commande, LigneCommande
+from .emplacement import Emplacement
+from .implantation import Implantation
+from .reception import Reception
+
+__all__ = [
+    "Mission",
+    "Article",
+    "Agent",
+    "Commande",
+    "LigneCommande",
+    "Emplacement",
+    "Implantation",
+    "Reception"
+]
